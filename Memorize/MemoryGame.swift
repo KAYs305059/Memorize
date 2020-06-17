737c7b18b77af32ef712d052a37d1a67528c03ac
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2020-06-17 15:08:50 +0200

    added memorygame file

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Khadar Ahmed Yusuf on 17/06/2020.
+//  Copyright © 2020 Khadar Ahmed Yusuf. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame {
+    var cards: Array<Card>
+    
+    struct Card {
+        
+    }
+}
